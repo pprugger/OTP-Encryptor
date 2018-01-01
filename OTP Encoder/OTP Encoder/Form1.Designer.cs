7e907fe70433afd96a478127d1671442284fc5c9
pprugger <pprugger@users.noreply.github.com>
2018-01-01 22:25:15 +0100

    Add files via upload

---
+++ OTP Encoder/OTP Encoder/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace OTP_Encoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Encrypt_File = new System.Windows.Forms.Button();
+            this.Decrypt_File = new System.Windows.Forms.Button();
+            this.Text_Box = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Encrypt_File
+            // 
+            this.Encrypt_File.Location = new System.Drawing.Point(12, 23);
+            this.Encrypt_File.Name = "Encrypt_File";
+            this.Encrypt_File.Size = new System.Drawing.Size(75, 23);
+            this.Encrypt_File.TabIndex = 1;
+            this.Encrypt_File.Text = "Encrypt File";
+            this.Encrypt_File.UseVisualStyleBackColor = true;
+            this.Encrypt_File.Click += new System.EventHandler(this.Encrypt_File_Click);
+            // 
+            // Decrypt_File
+            // 
+            this.Decrypt_File.Location = new System.Drawing.Point(12, 66);
+            this.Decrypt_File.Name = "Decrypt_File";
+            this.Decrypt_File.Size = new System.Drawing.Size(75, 23);
+            this.Decrypt_File.TabIndex = 2;
+            this.Decrypt_File.Text = "Decrypt File";
+            this.Decrypt_File.UseVisualStyleBackColor = true;
+            this.Decrypt_File.Click += new System.EventHandler(this.Decrypt_File_Click);
+            // 
+            // Text_Box
+            // 
+            this.Text_Box.Location = new System.Drawing.Point(109, 23);
+            this.Text_Box.Name = "Text_Box";
+            this.Text_Box.Size = new System.Drawing.Size(355, 209);
+            this.Text_Box.TabIndex = 3;
+            this.Text_Box.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(476, 247);
+            this.Controls.Add(this.Text_Box);
+            this.Controls.Add(this.Decrypt_File);
+            this.Controls.Add(this.Encrypt_File);
+            this.Name = "Form1";
+            this.Text = "OTP Encryptor";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Encrypt_File;
+        private System.Windows.Forms.Button Decrypt_File;
+        private System.Windows.Forms.RichTextBox Text_Box;
+    }
+}
+
---
+++ OTP Encoder/OTP Encoder/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace OTP_Encoder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Encrypt_File_Click(object sender, EventArgs e)
+        {
+            string full_file_path = String.Empty;
+            string filename = String.Empty;
+            string path = String.Empty;
+            string keyfile_name = String.Empty;
+            string keyfile_full_path = String.Empty;
+            string encrypted_file_name = String.Empty;
+            long filelength = 0;
+            byte[] key;
+            byte[] file;
+            byte[] encrypted_file;
+
+            RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
+            OpenFileDialog file_encode = new OpenFileDialog();
+
+            file_encode.InitialDirectory = "c:\\";
+            file_encode.Filter = "All files (*.*)|*.*";
+            file_encode.FilterIndex = 1;
+            file_encode.RestoreDirectory = true;
+
+            if (file_encode.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+
+                    full_file_path = file_encode.FileName;
+                    filename = Path.GetFileName(full_file_path);
+                    path = Path.GetDirectoryName(full_file_path);
+                    encrypted_file_name = filename + ".otp";
+                    keyfile_name = filename + ".otpkey";
+                    keyfile_full_path = path + "\\" + keyfile_name;
+
+                    FileInfo File_Info = new FileInfo(full_file_path);
+                    filelength = File_Info.Length;
+
+                    Text_Box.Clear();
+                    Text_Box.AppendText("Opening File: " + filename + " \n");
+                    Text_Box.AppendText("Size " + filelength + " Bytes \n");
+                    Text_Box.AppendText("Keyfile path is " + keyfile_full_path + "\n");
+                    Text_Box.AppendText("Encrypted filename is " + encrypted_file_name + "\n");
+                }
+                catch (Exception ex)
+                {
+                    Text_Box.AppendText("Error: Could not read file from disk. \n" + ex.Message);
+                    return;
+                }
+            }
+            else
+            {
+                return;
+            }
+
+            key = new byte[filelength];
+            file = new byte[filelength];
+            encrypted_file = new byte[filelength];
+            rngCsp.GetBytes(key);
+
+            try
+            {
+                Text_Box.AppendText("Reading file \n");
+                file = File.ReadAllBytes(full_file_path);
+            }
+            catch (Exception ex)
+            {
+                Text_Box.AppendText("Error: Could not read file from disk. \n" + ex.Message);
+                return;
+            }
+
+            for (int i = 0; i < filelength; i++)
+            {
+                encrypted_file[i] = (byte)(file[i] ^ key[i]);
+            }
+
+            try
+            {
+                Text_Box.AppendText("Writing keyfile \n");
+                File.WriteAllBytes(keyfile_full_path, key);
+            }
+            catch (Exception ex)
+            {
+                Text_Box.AppendText("Error: Could not write to disk. \n" + ex.Message);
+                return;
+            }
+
+            try
+            {
+                Text_Box.AppendText("Writing encrypted file \n");
+                File.WriteAllBytes(path + "\\" + encrypted_file_name, encrypted_file);
+            }
+            catch (Exception ex)
+            {
+                Text_Box.AppendText("Error: Could not write to disk. \n" + ex.Message);
+                return;
+            }
+            Text_Box.AppendText("Finished! \n");
+        }
+
+        private void Decrypt_File_Click(object sender, EventArgs e)
+        {
+            string full_file_path = String.Empty;
+            string filename = String.Empty;
+            string filename_without_extension = String.Empty;
+            string path = String.Empty;
+            string keyfile_full_path = String.Empty;
+            long filelength = 0;
+            byte[] key;
+            byte[] file;
+            byte[] decrypted_file;
+
+
+            OpenFileDialog file_decrypt = new OpenFileDialog();
+            file_decrypt.InitialDirectory = "c:\\";
+            file_decrypt.Filter = "OTP Files (*.otp)|*.otp";
+            file_decrypt.FilterIndex = 1;
+            file_decrypt.RestoreDirectory = true;
+
+            if (file_decrypt.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    full_file_path = file_decrypt.FileName;
+                    filename = Path.GetFileName(full_file_path);
+                    filename_without_extension = Path.GetFileNameWithoutExtension(full_file_path);
+                    path = Path.GetDirectoryName(full_file_path);
+                    keyfile_full_path = path + "\\" + filename_without_extension + ".otpkey";
+
+                    if (Path.GetExtension(full_file_path) != ".otp")
+                    {
+                        Text_Box.AppendText("Please choose an .otp file");
+                        return;
+                    }
+
+                    FileInfo File_Info = new FileInfo(full_file_path);
+                    filelength = File_Info.Length;
+
+                    Text_Box.Clear();
+                    Text_Box.AppendText("Opening File: " + filename + " \n");
+                    Text_Box.AppendText("Size " + filelength + " Bytes \n");
+                    Text_Box.AppendText("Keyfile path is " + keyfile_full_path + "\n");
+                }
+                catch (Exception ex)
+                {
+                    Text_Box.AppendText("Error: Could not read file from disk. \n" + ex.Message);
+                    return;
+                }
+            }
+            else
+            {
+                return;
+            }
+
+            key = new byte[filelength];
+            file = new byte[filelength];
+            decrypted_file = new byte[filelength];
+
+            try
+            {
+                Text_Box.AppendText("Reading file \n");
+                file = File.ReadAllBytes(full_file_path);
+            }
+            catch (Exception ex)
+            {
+                Text_Box.AppendText("Error: Could not read file from disk. \n" + ex.Message);
+                return;
+            }
+
+            try
+            {
+                Text_Box.AppendText("Reading keyfile \n");
+                key = File.ReadAllBytes(keyfile_full_path);
+            }
+            catch (Exception ex)
+            {
+                Text_Box.AppendText("Error: Could not read file from disk. \n" + ex.Message);
+                return;
+            }
+
+            for (int i = 0; i < filelength; i++)
+            {
+                decrypted_file[i] = (byte)(file[i] ^ key[i]);
+            }
+
+            try
+            {
+                Text_Box.AppendText("Writing decrypted file \n");
+                File.WriteAllBytes(path + "\\" + filename_without_extension, decrypted_file);
+            }
+            catch (Exception ex)
+            {
+                Text_Box.AppendText("Error: Could not write to disk. \n" + ex.Message);
+                return;
+            }
+            Text_Box.AppendText("Finished! \n");
+        }
+    }
+}
